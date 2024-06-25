7c9e3b45da8df2c06b8f65106eb36d5674481ceb
GitJIHO <jiho99322@gmail.com>
2024-06-25 23:13:47 +0900

    feat: GetMapping어노테이션 getProduct()로 저장된 Product객체들의 리스트를 반환하는 GET기능 구현

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,7 +1,10 @@
 package gift;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
     private long nextId = 1;
+
+    @GetMapping
+    public List<Product> getProduct(){
+        return new ArrayList<>(products.values());
+    }
 
 }
