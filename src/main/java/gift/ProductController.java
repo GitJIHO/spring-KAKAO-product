06d4ec5d2309cad20c81e3aa50570986fb205d0e
GitJIHO <jiho99322@gmail.com>
2024-06-25 23:50:27 +0900

    fix: addProduct에서 product의 id 설정 문법 오류 수정

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,7 +27,7 @@ public class ProductController {
 
     @PostMapping
     public Product addProduct(@RequestBody Product product){
-        product.id = nextId++;
+        product.setId(nextId++);
         products.put(product.id,product);
         return product;
     }
