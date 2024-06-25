3d31ad41dea97a5cde3be19ec43e62f54ad45a49
GitJIHO <jiho99322@gmail.com>
2024-06-25 23:23:11 +0900

    feat: PostMapping어노테이션 addProduct메소드로 전달받은 JSON데이터를 Product객체 형태로 변환하여 1을 더한 id를 저장하고 product객체를  리턴해주는 기능 구현

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +21,9 @@ public class ProductController {
         return new ArrayList<>(products.values());
     }
 
+    @PostMapping
+    public Product addProduct(@RequestBody Product product){
+        products.put(nextId++,product);
+        return product;
+    }
 }
