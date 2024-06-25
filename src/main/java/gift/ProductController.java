51594b8ce636b86927d3f0e571d8a5ad87be4b28
GitJIHO <jiho99322@gmail.com>
2024-06-25 23:28:26 +0900

    feat: PutMapping어노테이션 changeProduct메소드로 PathVariable로 전달받은 id와 일치하는 데이터가 있는지 확인 후 데이터 수정 기능 구현

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,6 +27,16 @@ public class ProductController {
     @PostMapping
     public Product addProduct(@RequestBody Product product){
         products.put(nextId++,product);
+        return product;
+    }
+
+    @PutMapping
+    public Product changeProduct(@PathVariable Long id, @RequestBody Product product){
+        if(!products.containsValue(id)){
+            throw new RuntimeException("id에 해당하는 객체가 없습니다");
+        }
+        product.id = id;
+        products.put(id, product);
         return product;
     }
 }
