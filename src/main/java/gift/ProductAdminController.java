9d065e1754ef6b9ae03f42dc206485f485a5ec82
GitJIHO <jiho99322@gmail.com>
2024-06-26 18:56:18 +0900

    feat: 관리자 페이지 생성을 위해 ProductAdminController생성, @Controller 어노테이션 적용

---
+++ src/main/java/gift/ProductAdminController.java
@@ -0,0 +1,18 @@
+package gift;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/products")
+public class ProductAdminController {
+    private final Map<Long, Product> products = new HashMap<>();
+    private long nextId = 1;
+
+
+}
