435ff483a8d6772f33ade7d43af6926d99fbf3b3
GitJIHO <jiho99322@gmail.com>
2024-06-30 20:32:56 +0900

    style: 코드 자동 정렬 수행

--- src/main/java/gift/ProductAdminController.java
+++ src/main/java/gift/ProductAdminController.java
@@ -15,32 +15,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin/products")
 public class ProductAdminController {
+
     private final Map<Long, Product> products = new HashMap<>();
     private long nextId = 1;
 
     @GetMapping
-    public String getAllProducts(Model model){
+    public String getAllProducts(Model model) {
         List<Product> productList = new ArrayList<>(products.values());
         model.addAttribute("products", productList);
         return "product-list";
     }
 
     @GetMapping("/add")
-    public String addProductForm(Model model){
+    public String addProductForm(Model model) {
         model.addAttribute("product", new Product());
         return "product-form";
     }
 
     @PostMapping("/add")
-    public String addProduct(@ModelAttribute Product product){
+    public String addProduct(@ModelAttribute Product product) {
         product.setId(nextId++);
         products.put(product.getId(), product);
         return "redirect:/admin/products";
     }
 
     @GetMapping("edit/{id}")
-    public String updateProductForm(@PathVariable("id") Long id, Model model){
-        if(!products.containsKey(id)){
+    public String updateProductForm(@PathVariable("id") Long id, Model model) {
+        if (!products.containsKey(id)) {
             throw new RuntimeException("해당 id를 가지고있는 Product 객체가 없습니다.");
         }
         model.addAttribute("product", products.get(id));
@@ -48,15 +49,15 @@ public class ProductAdminController {
     }
 
     @PostMapping("edit/{id}")
-    public String updateProduct(@PathVariable("id") Long id, @ModelAttribute Product product){
+    public String updateProduct(@PathVariable("id") Long id, @ModelAttribute Product product) {
         product.setId(id);
         products.put(product.getId(), product);
         return "redirect:/admin/products";
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable("id") Long id){
-        if(!products.containsKey(id)){
+    public String deleteProduct(@PathVariable("id") Long id) {
+        if (!products.containsKey(id)) {
             throw new RuntimeException("해당 id를 가지고있는 Product 객체가 없습니다.");
         }
         products.remove(id);
